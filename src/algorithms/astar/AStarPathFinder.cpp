08126f24dc6b937e2f5c798f8632d868a9cc8066
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-03-22 14:46:22 +0200

    added skeleton for AStarPathFinder

---
+++ src/algorithms/astar/AStarPathFinder.cpp
@@ -0,0 +1,5 @@
+#include "AStarPathFinder.h"
+
+namespace pathfinder {
+
+}
---
+++ src/algorithms/astar/AStarPathFinder.h
@@ -0,0 +1,20 @@
+/**
+ * @brief AStarPathFinder class.
+ *
+ * Class for finding the shortest path with using A* algorithm.
+ *
+ * @author Valtteri Pyyhtiä
+ */
+
+#ifndef A_STAR_PATH_FINDER_H
+#define A_STAR_PATH_FINDER_H
+
+namespace pathfinder {
+
+    class AStarPathFinder {
+        // empty
+    };
+
+}
+
+#endif
