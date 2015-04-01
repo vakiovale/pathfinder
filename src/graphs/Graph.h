2098df644239e9cfa6d6903c63371c607648fb42
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-04-01 21:32:44 +0300

    added comments to the Graph

--- src/graphs/Graph.h
+++ src/graphs/Graph.h
@@ -18,11 +18,51 @@ namespace pathfinder {
             std::vector<std::vector<Node>> nodes;
         public:
             Graph();
+
+            /**
+             * @brief Graph will initialize 2D Grid Map
+             *
+             * Graph will get a 2D grid map with a specific side length.
+             *
+             * @param lengthOfSide
+             */
             void create2DGridMap(int lengthOfSide);
+
+            /**
+             * @brief Returns all the nodes from the graph
+             *
+             * @return vector of nodes
+             */
             std::vector<std::vector<Node>> getAllNodes() const;
+
+            /**
+             * @brief Return a pointer to a node from a specific location
+             *
+             * @param x position
+             * @param y position
+             * @return pointer to Node
+             */
             const Node* getNodeFromPosition(int x, int y);
+
+            /**
+             * @brief Check if Node exists in a specific position
+             *
+             * @param x position
+             * @param y position
+             * @return true if Node exists in a graph
+             */
             bool nodeExistsInPosition(int x, int y) const;
+
+            /**
+             * @brief Return number of nodes in the graph
+             *
+             * @return number of nodes
+             */
             int getNumberOfNodes() const;
+
+            /**
+             * @brief Prints nodes
+             */
             void printNodes() const;
     };
 
