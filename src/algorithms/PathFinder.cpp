7ed33d7d0056c2e7933621b62ef159dd8a4a172b
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-04-01 22:49:27 +0300

    working on PathFinder

--- src/algorithms/PathFinder.cpp
+++ src/algorithms/PathFinder.cpp
@@ -1,1 +1,9 @@
 #include "PathFinder.h"
+
+namespace pathfinder {
+
+    PathFinder::PathFinder(Graph* graph) : GRAPH(graph) {
+        // nothing more to be done
+    }
+
+}
--- src/algorithms/PathFinder.h
+++ src/algorithms/PathFinder.h
@@ -1,4 +1,18 @@
 #ifndef PATH_FINDER_H
 #define PATH_FINDER_H
 
+#include "../graphs/Graph.h"
+
+namespace pathfinder {
+
+    class PathFinder {
+
+        private:
+            const Graph* const GRAPH;
+        public:
+            PathFinder(Graph* graph);
+    };
+
+}
+
 #endif
