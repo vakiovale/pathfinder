e62f2e47202cc960ad2f2d1e12650c3f538c91cd
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-04-16 15:57:15 +0300

    converted PriorityQueue class to template class

---
+++ src/utilities/PriorityQueue.h
@@ -0,0 +1,20 @@
+/**
+ * @brief PriorityQueue
+ *
+ * Implements priority queue.
+ *
+ * @author Valtteri Pyyhtiä
+ */
+
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
+
+namespace pathfinder {
+
+    template <typename T> class PriorityQueue {
+
+    };
+
+}
+
+#endif
