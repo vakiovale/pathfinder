f7f6e063e7bf190683c2b48143143a53fda8856f
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-04-16 15:27:08 +0300

    organized functions for easier reading

--- src/pathtools/Heuristic.cpp
+++ src/pathtools/Heuristic.cpp
@@ -15,23 +15,8 @@ namespace pathfinder {
         return estimation;
     }
 
-    void Heuristic::setDiagonalMovementCostFactor(
-            float diagonalMovementCostFactor) {
-        this->nonDiagonalMovementCost = 1.0f;
-        this->diagonalMovementCost =
-                nonDiagonalMovementCost * diagonalMovementCostFactor;
-    }
-
     int Heuristic::calculateDifference(int firstValue, int secondValue) const {
         return std::abs(firstValue - secondValue);
-    }
-
-    float Heuristic::calculateNonDiagonalMovementCost(int firstDelta, int secondDelta) const {
-        return nonDiagonalMovementCost * (firstDelta + secondDelta);
-    }
-
-    float Heuristic::calculateDiagonalMovementSave() const {
-        return diagonalMovementCost - 2 * nonDiagonalMovementCost;
     }
 
     float Heuristic::calculateDiagonalDistanceEstimation(int firstDelta,
@@ -45,4 +30,19 @@ namespace pathfinder {
         return nonDiagonalMovementCost + diagonalMovementSave * smallerDelta;
     }
 
+    float Heuristic::calculateNonDiagonalMovementCost(int firstDelta,
+                                                      int secondDelta) const {
+        return nonDiagonalMovementCost * (firstDelta + secondDelta);
+    }
+
+    float Heuristic::calculateDiagonalMovementSave() const {
+        return diagonalMovementCost - 2 * nonDiagonalMovementCost;
+    }
+
+    void Heuristic::setDiagonalMovementCostFactor(
+            float diagonalMovementCostFactor) {
+        this->nonDiagonalMovementCost = 1.0f;
+        this->diagonalMovementCost =
+                nonDiagonalMovementCost * diagonalMovementCostFactor;
+    }
 }
