5efee573005a0d9ea35e0917d47baa7a3acd9e53
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-04-16 15:20:27 +0300

    updated comments to Heuristic class

--- src/pathtools/Heuristic.h
+++ src/pathtools/Heuristic.h
@@ -16,6 +16,42 @@ namespace pathfinder {
 
     class Heuristic {
 
+        public:
+            /**
+             * @brief Heuristic
+             *
+             * Initializes Heuristic to be used in pathfinding.
+             * Also initializes diagonal movement cost factor to 1.4f.
+             *
+             */
+            Heuristic();
+
+            /**
+             * @brief Estimate distance between two nodes
+             *
+             * Estimates distance between two nodes calculating the
+             * diagonal distance. Diagonal distance will be zero if the
+             * starting node and end node is the same and greater than zero
+             * if they are different.
+             *
+             * @param start node
+             * @param end node
+             * @return diagonal distance between two nodes
+             */
+            float estimateDistance(Node start, Node end) const;
+
+            /**
+             * @brief Set diagonal movement cost factor
+             *
+             * Diagonal movement cost factor indicates how much does it cost
+             * when moving diagonally compared to moving non-diagonally.
+             * If diagonal movement cost factor is set to 1.0f then it's as
+             * expensive to move diagonally than non-diagonally.
+             *
+             * @param diagonalMovementCostFactor
+             */
+            void setDiagonalMovementCostFactor(float diagonalMovementCostFactor);
+
         private:
             float nonDiagonalMovementCost;
             float diagonalMovementCost;
@@ -28,11 +64,6 @@ namespace pathfinder {
 
             float calculateDiagonalDistanceEstimation(int firstDelta,
                                                       int secondDelta) const;
-
-        public:
-            Heuristic();
-            float estimateDistance(Node start, Node end) const;
-            void setDiagonalMovementCostFactor(float diagonalMovementCostFactor);
     };
 }
 
