6f3ea9653f60bb8e7b8aa520f2c2be739ce8be7f
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-05-08 19:42:28 +0300

    sfml installation

--- pathfinder-demo/src/main.cpp
+++ pathfinder-demo/src/main.cpp
@@ -1,12 +1,23 @@
-#include "Node.h"
-#include <iostream>
+#include "SFML/Graphics.hpp"
 
-using namespace pathfinder;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-int main() {
-
-    Node node(2,1);
-    std::cout << "Demo node (" << node.getX() << "," << node.getY() << ")" << std::endl;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
     return 0;
-
 }
